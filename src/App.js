09cf5967b800543a6cf854251d67ca1dc0821268
vaibhavdixit107 <vaibhavdixit107@gmail.com>
2023-08-03 14:01:42 -0400

    Add validation and improve UI

--- src/App.js
+++ src/App.js
@@ -1,46 +1,87 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Button, TextField, Grid } from '@mui/material';
+import validator from 'validator'
+
 
 function App() {
   const [url, setUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
+  const [isValidUrl, setIsValidUrl] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (isValidURL) {
+      try {
+        const response = await axios.post('http://localhost:5000/shortUrl', {
+          fullUrl: url
+        });
 
-    try {
-      const response = await axios.post('http://localhost:5000/shortUrl', {
-        fullUrl: url
-      });
-
-      setShortUrl(response.data.shortUrl);
-    } catch (error) {
-      console.error('Error creating short URL', error);
+        setShortUrl(response.data.shortUrl);
+      } catch (error) {
+        console.error('Error creating short URL', error);
+      }
     }
+
+  };
+  const isValidURL = (url) => {
+    return validator.isURL(url)
   };
 
+  const handleChange = (event) => {
+    const inputUrl = event.target.value;
+    setUrl(inputUrl);
+    setIsValidUrl(isValidURL(inputUrl));
+  }
+
+  const handleClear = () => {
+    setUrl('')
+  }
   return (
-    <div className="App">
-      <h1>URL Shortener</h1>
+    <Grid container
+      spacing={2}
+      direction="column"
+      justifyContent="center"
+      alignItems="center">
+      <Grid item xs={8}>
+        <h1>URL Shortener</h1>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={url}
-          onChange={e => setUrl(e.target.value)}
-          placeholder="Enter URL here"
-          required
-        />
-        <button type="submit">Shorten URL</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Enter a valid URL here"
+            value={url}
+            onChange={handleChange}
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            error={!isValidUrl && url !== ''}
+            helperText={!isValidUrl && url !== '' ? 'Invalid URL format' : ''}
+          />
 
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item>
+              <Button variant="contained" color="primary" type="submit" disabled={!isValidUrl}>
+                Submit
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" type='reset' onClick={handleClear}>
+                Clear
+              </Button>
+            </Grid>
+          </Grid>
+
+        </form>
+
+
+      </Grid>
       {shortUrl && (
         <div>
           <h3>Short URL:</h3>
           <a href={shortUrl} target="_blank" rel="noreferrer">{shortUrl}</a>
         </div>
       )}
-    </div>
+    </Grid>
   );
 }
 
